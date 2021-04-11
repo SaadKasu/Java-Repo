7653f60c262be3b2f713e6837715917858507f4f
SaadKasu <saadleokasu@gmail,com>
2021-04-11 14:26:47 +0530

    Created a Java File

---
+++ Print.java
@@ -0,0 +1,7 @@
+import java.util.*;
+import java.lang.*;
+Class Print{
+    public static void main(String[] args){
+        System.out.Println("Hi I have created this on the Local Machine");
+    }
+}
